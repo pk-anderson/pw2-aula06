2be4dbcf7c0e4cdf5fe5282b957e2566b782e8cb
Patrick Anderson <71664004+pk-anderson@users.noreply.github.com>
2023-04-14 20:55:54 -0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,58 @@
-import {Cidade} from "func.ts"
+import {arraysEqual, equalElements, arraysOfArraysEqual, compareArrays, Cidade, descreveCidade, Restaurante, agruparRestaurantesPorCidade} from './index'
 
 //Questão 1
 
-const a = ['abc', 'def', 'ghi'];
-const b = ['ghi', 'abc', 'def']; 
+var firstArray = ['eu','tu','ele']
+var secondArray = ['nos','vos','eles']
+console.log(arraysEqual(firstArray,secondArray))
+var firstArray = ['eu','tu','ele']
+var secondArray = ['eu','tu','ele']
+console.log(arraysEqual(firstArray,secondArray))
 
-console.log(Cidade)
+//Questão 2
+
+var firstArray = ['eu','tu']
+var secondArray = ['eu','tu','ele']
+console.log(equalElements(firstArray,secondArray))
+var firstArray = ['eu','tu','ele']
+var secondArray = ['eu','tu','eles']
+console.log(equalElements(firstArray,secondArray))
+
+//Questão 3
+
+const a: string[][] = [['a', 'b'], ['c', 'd']]
+const b: string[][] = [['a', 'b'], ['c', 'd']]
+const c: string[][] = [['a', 'b'], ['d', 'c']]
+console.log(arraysOfArraysEqual(a, b))
+console.log(arraysOfArraysEqual(a, c))
+
+//Questão 4
+
+const d: string[][] = [['a', 'b'], ['c', 'd']]
+const e: string[][] = [['a', 'b'], ['d', 'c']]
+const result = compareArrays(d, e)
+console.log(result)
+
+//Questão 5
+
+const cidade: Cidade = {
+    alcunha: "A terra que ensinou a Paraíba a ler.",
+    coordenadas: {
+      latitude: [6, 53, 24],
+      longitude: [38, 33, 43],
+    },
+    nome: "Cajazeiras",
+  };
+  
+  console.log(descreveCidade(cidade));
+  
+//Questão 6
+
+const restaurantes: Restaurante[] = [
+    { cidade: "Cajazeiras", nome: "Burgdega" },
+    { cidade: "Sousa", nome: "Dino's" },
+    { cidade: "Cajazeiras", nome: "Avenida" },
+];
+  
+const resultado = agruparRestaurantesPorCidade(restaurantes);
+console.log(resultado);
